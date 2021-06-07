10c0565116c23026475ee71a97f42c15fc46dfdc
veinpale13 <85495991+veinpale13@users.noreply.github.com>
2021-06-07 17:53:19 +0300

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,16 @@
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+  console.log(`[BOT] | Botun destekçileri ve komutları başarıyla yüklendi, işlemler tamamlandı !`);
+  console.log(`[BOT] | (${client.user.username}) UZZY
+  ‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒`);
+  client.user.setStatus("online");
+  client.user.setActivity("Veinpale♥Uzzy", { type: "TYPE"}); //// TYPE - WATCHING , PLAYING , LISTENING gibi değiştirilebilir.
+  console.log(`ABILERE SELAM UZZY DEWAM`);
+
+};
