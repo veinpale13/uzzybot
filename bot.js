057d60c59766e5306f1d9532b69d40defe7dedbd
veinpale13 <85495991+veinpale13@users.noreply.github.com>
2021-06-07 22:00:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -447,7 +447,7 @@ client.on('messageDelete', message => {
 })
 
 client.on("ready", () =>{
-    client.channels.cache.get('837030779451211827').join()
+    client.channels.cache.get('843587013227446302').join()
     })
 
 
